2840934f11e379207b8ddd9213b8f8abf55af562
lucns <luckyfire1996@gmail.com>
2018-06-23 22:37:57 +0700

    LucNS: update version gradle

--- app/src/main/java/com/dentalclinic/capstone/firebase/FirebaseMessageService.java
+++ app/src/main/java/com/dentalclinic/capstone/firebase/FirebaseMessageService.java
@@ -1,50 +1,50 @@
-//package com.dentalclinic.capstone.firebase;
-//
-//import android.app.NotificationManager;
-//import android.app.PendingIntent;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.media.RingtoneManager;
-//import android.net.Uri;
-//import android.support.v4.app.NotificationCompat;
-//import android.util.Log;
-//
-//import com.dentalclinic.capstone.R;
-//import com.dentalclinic.capstone.activities.MainActivity;
-//import com.dentalclinic.capstone.utils.AppConst;
-//import com.google.firebase.iid.FirebaseInstanceId;
-//import com.google.firebase.messaging.FirebaseMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-//
-//public class FirebaseMessageService extends FirebaseMessagingService {
-//    @Override
-//    public void onMessageReceived(RemoteMessage message) {
-//        sendMyNotification(message.getNotification().getBody());
-//        Log.d(AppConst.DEBUG_TAG, "Received");
-//    }
-//
-//
-//    private void sendMyNotification(String message) {
-//
-//        //On click of notification it redirect to this Activity
-//
-//        Intent intent = new Intent(this, MainActivity.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-//
-//
-//        Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//                .setContentTitle("My Firebase Push notification")
-//                .setContentText(message)
-//                .setAutoCancel(true)
-//                .setSound(soundUri)
-//                .setContentIntent(pendingIntent);
-//
-//        NotificationManager notificationManager =
-//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        notificationManager.notify(0, notificationBuilder.build());
-//    }
-//}
+package com.dentalclinic.capstone.firebase;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.dentalclinic.capstone.R;
+import com.dentalclinic.capstone.activities.MainActivity;
+import com.dentalclinic.capstone.utils.AppConst;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessageService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        sendMyNotification(message.getNotification().getBody());
+        Log.d(AppConst.DEBUG_TAG, "Received");
+    }
+
+
+    private void sendMyNotification(String message) {
+
+        //On click of notification it redirect to this Activity
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+
+        Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My Firebase Push notification")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
